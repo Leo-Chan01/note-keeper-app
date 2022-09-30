a885431b48c75d139665212e96c5d792351094ea
Leo-Chan01 <ephraimleo16@gmail.com>
2022-09-30 11:28:24 +0100

    [ADD] NoteListActivity.kt
    - Add Activity to display the list of Note
    - Change configuration of Manifest Files

---
+++ app/src/main/java/com/raym/notekeeper/NoteListActivity.kt
@@ -0,0 +1,27 @@
+package com.raym.notekeeper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.snackbar.Snackbar
+import com.raym.notekeeper.databinding.ActivityNoteListBinding
+
+class NoteListActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityNoteListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNoteListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
